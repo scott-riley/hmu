2c08eec0e47fea4f19b2941f62d4f9fce4f2d379
Scott Riley <hugs@scottriley.im>
2016-03-16 19:37:35 +0000

    Copy changes.

--- src/components/Site/Hero/Hero.js
+++ src/components/Site/Hero/Hero.js
@@ -15,7 +15,7 @@ export default class Hero extends Component {
             Hit me up, m8
           </h1>
           <p className={[s.copy].join(' ')}>
-            Everyone hates mailto: links. No one likes monolithic CRMs except marketing weirdos. HMU is a data-agnostic API. It lets you receive and manage your enquiries without creating your own endpoint. And it’s free, you lucky thing.
+            Everyone hates mailto: links. No one likes monolithic CRMs except marketing weirdos. HMU is a data-agnostic API. It lets you receive and manage your enquiries without coding and managing your own endpoint. And it’s free, you lucky thing.
           </p>
           <p className={s.linkContainer}>
             <Link to="/app/signup" className={s.link}>Get started for free</Link>
--- src/components/Site/HowItWorks/HowItWorks.js
+++ src/components/Site/HowItWorks/HowItWorks.js
@@ -11,7 +11,7 @@ export default class HowItWorks extends Component {
           <h2 className={s.subhead}>How it works</h2>
         </div>
         <div className={s.smallWrap}>
-          <p><span className={s.emoji}>🕶</span> Sign up and get your own super cool URL</p>
+          <p><span className={s.emoji}>🕶</span> Sign up and get your own super cool URL.</p>
           <p><span className={s.emoji}>💌</span> Create a form, or an app, or whatever you like, that sends data to this URL.</p>
           <p><span className={s.emoji}>💬</span> Start receiving messages. They’ll be shown on your dashboard, and any fields you send from your form will determine how your message data is structured.</p>
           <p><Link to="/docs" className={s.link}>Detailed instructions</Link></p>
